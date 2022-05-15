8a8399011432f540c094df155d0a8e8bbd3c1342
Sachin Nimshan <sachinnimshan@gmail.com>
2022-05-15 15:21:16 +0530

    latest

---
+++ src/components/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Grid} from '@mui/material';
+
+function Home() {
+  return (
+    <Grid container xs={12}>
+        <span>Sachin Nimshan</span>
+    </Grid>
+  )
+}
+
+export default Home;
