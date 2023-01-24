76db83fcc75096457c8a2118525ff22d3b7e95ca
Sachin Nimshan <sachinnimshan@gmail.com>
2023-01-24 14:20:41 +0530

    latest

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
+import Header from "./components/header";
 
-import './App.scss';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Home from './components/home/Home';
 
 function App() {
   return (
     <div className="App">
-     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-      </Routes>
-     </BrowserRouter>
+      <Header/>
+      Hello
     </div>
   );
 }
---
+++ src/components/header/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {} from './header.styled';
+
+function Header() {
+  return <div>index</div>;
+}
+
+export default Header;
